4e7ee6d719fdbc7a7e229acbe3c69b7ecf8c1fb7
tawatson02 <ta_watson@live.com>
2024-03-28 22:18:20 -0500

    wanted me to hide bearer token

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 //     method: 'GET',
 //     headers: {
 //       accept: 'application/json',
-//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YWM4NDczNjRjZDA2NGVmYjdlNDc5YzUzY2UwZTgwOSIsInN1YiI6IjY2MDYyOWVhMmZhZjRkMDE3ZGM2YWZiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nCbINUDrRR-6SQ0MpGHIXb7X37u2Ot-mUmJ2sBELFHo'
 //     }
 //   };
   
